dea6a4cfd750ff5f56091c5f0081bfc47fcc93c6
root <Nicolaslegoat>
2023-07-25 08:25:55 -0700

    add structure in main.h

--- main.h
+++ main.h
@@ -1,8 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: the function associed
+ *
+ */
+
+typedef struct specifiers
+{
+	char specifiers;
+	int (*f)(va_list);
+} spec_t;
+
 int _myputchar(char c);
 int _printf(const char *format, ...);
-int _string(char *str);
-int _integers(int num);
+int get_func( char conv, va_list args);
+
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_mod(va_list args);
+int print_num(va_list args);
+
 #endif
