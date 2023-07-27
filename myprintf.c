44dea2eb42d32051ca28831e6a675d30bfe226d9
root <Nicolaslegoat>
2023-07-27 01:14:15 -0700

    add format == NULL

--- myprintf.c
+++ myprintf.c
@@ -10,6 +10,10 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+
+	if (format == NULL)
+		return (-1);
+
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
